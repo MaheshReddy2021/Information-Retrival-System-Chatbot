a7ed622cc5ae8e36685731cc163d333ed3dc56d6
MaheshReddy2021 <johnny1603267@gmail.com>
2024-08-09 23:22:08 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import find_packages,setup
+
+setuptools.setup(
+    
+    name = 'Generative AI project',
+    version ='0.0.0',
+    author='MaheshReddy',
+    author_email='johnny1603267@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+  
+  
+  
+    
+)
